ec11994ffa8250685054819e928801b3e0e54867
ShaunLiou <weisyuanliou@gmail.com>
2020-05-06 16:15:23 +0800

    changes update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Timezone from './components/header/header';
+// import App from './App';
+// import Demo from './components/scheduler';
+// import WeekNav from './components/datetime';
+import Content from './components/content/content';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+// import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+      <div>
+        {/* <WeekNav/> */}
+        <Content />
+        {/* <Demo /> */}
+        {/* <Timezone/> */}
+      </div>
+     
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
